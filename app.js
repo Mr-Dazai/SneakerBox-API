4a8eb8413b82e5ba5425624d8d1c54c39d730f2e
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-09-07 02:16:26 -0400

    Register Route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 require('express-async-errors')
 
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 //Express import
 const express = require('express')
@@ -22,12 +23,17 @@ const errorhandler = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
 
 // Get()
 app.get('/', (req, res) => {
   res.send('E-Commerse App')
 })
 
+app.get('/api/v1', (req, res) => {
+  console.log(req.signedCookies)
+  res.send('E-Commerse App')
+})
 app.use('/api/v1/auth', authRouter)
 // Error Handleing
 app.use(notFoundMiddleware)
--- cotrollers/authController.js
+++ cotrollers/authController.js
@@ -1,5 +1,33 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+const { attachCookiesToResponse, createTokenUser } = require('../utils')
+// const { createJWT } = require('../utils')
+
 const register = async (req, res) => {
-  res.send('register user')
+  const { email, name, password } = req.body
+  const emailAlreadyExists = await User.findOne({ email })
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email Already Exists')
+  }
+  const isFirstAccount = (await User.countDocuments({})) == 0
+  const role = isFirstAccount ? 'admin' : 'user'
+  const user = await User.create({ name, email, password, role })
+
+  // const tokenUser = { name: user.name, userID: user._id, role: user.role }
+  // const token = createJWT({ payload: tokenUser })
+
+  // const oneDay = 1000 * 60 * 60 * 24
+
+  // res.cookie('token', token, {
+  //   httpOnly: true,
+  //   expires: new Date(Date.now() + oneDay),
+  //   secure: process.env.NODE_ENV === 'production',
+  //   signed: true,
+  // })
+  const tokenUser = createTokenUser(user)
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 const login = async (req, res) => {
   res.send('Login')
