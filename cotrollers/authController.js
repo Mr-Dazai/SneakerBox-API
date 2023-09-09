8f37082c2d5c99857e14ba98de17c2edde978cc3
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-09-08 21:19:38 -0400

    login logout

--- cotrollers/authController.js
+++ cotrollers/authController.js
@@ -2,7 +2,6 @@ const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 const { attachCookiesToResponse, createTokenUser } = require('../utils')
-// const { createJWT } = require('../utils')
 
 const register = async (req, res) => {
   const { email, name, password } = req.body
@@ -13,27 +12,34 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) == 0
   const role = isFirstAccount ? 'admin' : 'user'
   const user = await User.create({ name, email, password, role })
-
-  // const tokenUser = { name: user.name, userID: user._id, role: user.role }
-  // const token = createJWT({ payload: tokenUser })
-
-  // const oneDay = 1000 * 60 * 60 * 24
-
-  // res.cookie('token', token, {
-  //   httpOnly: true,
-  //   expires: new Date(Date.now() + oneDay),
-  //   secure: process.env.NODE_ENV === 'production',
-  //   signed: true,
-  // })
   const tokenUser = createTokenUser(user)
   attachCookiesToResponse({ res, user: tokenUser })
   res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 const login = async (req, res) => {
-  res.send('Login')
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Enter Email and Password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Email or Password is Invalid')
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials')
+  }
+  const tokenUser = createTokenUser(user)
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 const logout = async (req, res) => {
-  res.send('Logout')
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  })
+  res.status(StatusCodes.OK).json({ msg: 'user logout' })
 }
 
 module.exports = {
