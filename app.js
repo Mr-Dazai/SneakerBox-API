8a182188050a932d878099a87ff58560fa133ba5
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-09-10 21:04:17 -0400

    product routes

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ require('dotenv').config()
 //Express-Aysnc import
 require('express-async-errors')
 
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
-
 //Express import
 const express = require('express')
 const app = express()
+
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
 
 //Connect Database
 const connectDB = require('./db/connect')
 
 //Routers
 const authRouter = require('./routes/authRoutes')
+const userRouter = require('./routes/userRouter')
+const productRouter = require('./routes/productRoutes')
 
 //Middleware import
 const notFoundMiddleware = require('./middleware/not-found')
@@ -24,6 +27,8 @@ const errorhandler = require('./middleware/error-handler')
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
+app.use(fileUpload())
 
 // Get()
 app.get('/', (req, res) => {
@@ -35,6 +40,8 @@ app.get('/api/v1', (req, res) => {
   res.send('E-Commerse App')
 })
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productRouter)
 // Error Handleing
 app.use(notFoundMiddleware)
 app.use(errorhandler)
